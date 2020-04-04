5d4f79513c5b46f309b99b6e25cce7c393b54cfd
SevenXuxiaowen <xiaowen.xu.china@gmail.com>
2020-04-03 21:19:24 -0700

    update project

---
+++ src/CMS/blogs.js
@@ -0,0 +1,65 @@
+export const DATA = {
+    about:{
+        self_description: [
+            "I'm a software developer based in Bay area.",
+            "Graduated from Carnegie Mellon University, I develop and design exceptional websites and applications that provide intuitive, pixel-perfect user interfaces with efficient and modern backends."
+        ]
+    },
+    skills:{
+        skill_set:["Java","Javascript","Node.js","React","Redux","MongoDB","HTML/CSS","Bootstrap","semanticUI","d3.js"]
+    },
+    works:[
+        {
+            title: "User-auth APIs",
+            auth: "Adaviv Inc.",
+            tag:["Module", "server"],
+            stack:["Back-end", "node.js", "Google+", "session", "MongoDB", "passport.js"],
+            gitHub: "https://github.com/SevenXuxiaowen/user-auth-server",
+            img:"img/1-01.png",
+            
+            short_description: "Back-end module (server) development for user authentication. Based on session control, this module supports multiple 3rd party auth strategies, including Google, Facebook, etc."
+
+        },
+        {
+            title: "Annotation Web Tool",
+            auth: "Adaviv Inc.",
+            tag:["Full-stack development", "Web"],
+            stack:["Full-Stack", "Node.js", "jQuery", "semantic UI", "paper.js"],
+            gitHub: "https://github.com/SevenXuxiaowen/Cannabis-Annotator",
+            demo:"https://p3.adavivxiaowenx.now.sh/",
+            img:"img/2-01.png",
+
+            short_description: "Online free-drawing annotation tool, including multi-layer handler, box, brush and filters. The editor supports JSON / PNG / SVG export."
+        },{
+            title: "NLP Chatbot",
+            auth: "personal",
+            tag:["Full-stack development", "Web"],
+            stack:["Full-Stack","DialogFlow API", "Node.js", "Materialize"],
+            gitHub: "https://github.com/SevenXuxiaowen/Cannabis-Annotator",
+            demo:"https://pure-taiga-37357.herokuapp.com/",
+            img:"img/3-01.png",
+
+            short_description: "Embedded NLP Chatbot"
+        },{
+            title: "Trauma Forest",
+            auth: "personal",
+            tag:["Full-Stack development", "Web"],
+            stack:["Node.js", "Express", "MongoDB", "React.js", "semantic UI"],
+            gitHub: "https://github.com/SevenXuxiaowen/Trauma-Forest",
+            short_description: "Web2.0 development"
+        },{
+            title: "Smart Items",
+            auth: "personal",
+            tag:["Full-stack development", "Web"],
+            stack:["Full-Stack", "Node.js", "Express", "MongoDB", "React.js", "Bootstrap"],
+            gitHub: "https://github.com/SevenXuxiaowen/smart-items",
+            short_description: "Walmart product search and recommendation engine."
+        }
+    ],
+    contact:{
+        phone: "(+1) 412 419 5890",
+        email:"xiaowenxu1992@gmail.com",
+        address:"Fremont, CA 94536"
+    }
+
+}
